3f45bec038da90cf22d78c0af792a4da5b05a564
Deepak Oraon <deepakoraon0348@gmail.com>
2026-04-06 08:24:53 +0530

    second-commit

--- Data_type/index.py
+++ Data_type/index.py
@@ -1,4 +1,11 @@
+import math
+import random
 # ==================== data type=======================
 # Power 
-print(2**3);
+# print(2**3);
 
+# it give me random numbers.
+# print(random.random())  
+
+
+print("Hello Akaxsh")
