707ff482b1b3272555a076bcf5f63a271b7773c6
Deepak Oraon <deepakoraon0348@gmail.com>
2026-04-11 12:07:24 +0530

    slicing string

--- Data_type/index.py
+++ Data_type/index.py
@@ -8,4 +8,15 @@ import random
 # print(random.random())  
 
 
-print("Hello Akaxsh")
+# print("Hello Akaxsh")
+# ============ looping throught a string ================
+# for x in "banana":
+#   print(x, end=" ")
+
+# print(" ".join("banana"))
+# =======================slicing string ==========
+b = "Hello, World!"
+# print(b[2:len(b)+1])
+# print(b[:5]); slicing from start
+print(b[2:]);
+print(b[-5:-2]);
