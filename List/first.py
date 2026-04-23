bddeae9ccd4101fa003528993bbc8d68f99c18e6
Deepak Oraon <deepakoraon0348@gmail.com>
2026-04-23 10:25:46 +0530

    2nd commit

---
+++ List/first.py
@@ -0,0 +1,20 @@
+# my_list = [1, 4, 3]
+
+# my_list.append(2)
+# my_list.append(5)
+
+# print(my_list)
+# new_list = sorted(my_list)
+# print(new_list)
+# b = list((4, 5, 6))
+# print(b)
+
+s = {3, 1, 2}
+# print(s)
+
+# s = {"apple", "banana", "cherry"}
+# print(s)
+
+s.add(10)          # add one
+s.update([20,30])  # add multiple
+print(s)
---
+++ patterns/01_rectangle.py
@@ -0,0 +1,15 @@
+# def pattern(n):
+#     for i in range(n):
+#         for j in range(n):
+#             print("*", end="")
+
+#         print()
+
+# n=6
+# pattern(n)
+
+def pattern(n):
+    for i in range(n):
+        print("*" * n)
+
+pattern(6)
